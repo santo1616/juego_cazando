2cb9f1342273ce082bf39de1503bee220c601718
santo1616 <santo1616@gmail.com>
2026-04-16 23:40:14 -0500

    Agregado Variables COnstantes y posicion de gato y comida

--- cazando.js
+++ cazando.js
@@ -1,8 +1,41 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
+
+const ALTO_GATO=80;
+const ANCHO_GATO=80;
+const ALTO_COMIDA=40;
+const ANCHO_COMIDA=40;
+
+
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+
+
+
+function iniciarJuego(){
+    
+    gatoX = canvas.width / 2 - ANCHO_GATO / 2;
+    gatoY = canvas.height / 2 - ALTO_GATO / 2;
+
+    comidaX = canvas.width - ANCHO_COMIDA
+    comidaY = canvas.height -ALTO_COMIDA
+
+    graficarComida();
+    graficarGato();
+
+}
 function graficarGato(){
 
     ctx.fillStyle="green";
-    ctx.fillRect(canvas.width/2,canvas.height/2,40,60);
+    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO);
 }
+
+function graficarComida(){
+
+    ctx.fillStyle="yellow";
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);
+}
+
